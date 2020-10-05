e649d2d0f2610dae8a76e883e8b2d0602c17a72a
Josef Burchert <josef.burchert@solutions2share.net>
2020-10-05 16:41:39 +0200

    Bug fixing

--- src/controller/Utilities.ts
+++ src/controller/Utilities.ts
@@ -2,7 +2,7 @@
 
 export default class Utilities {
   public static createMailFileName(subject: string): string {
-    let fileName = subject.replace(/ /g, '_').replace(/:/g, '_');
+    let fileName = subject.replace(/ /g, '-').replace(/:/g, '_').replace(/"/g, '');
     return fileName;
   }
 }
